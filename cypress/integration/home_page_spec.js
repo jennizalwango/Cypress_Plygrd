fe79dd64e7e598b42baa59b259d46f52ffd9027b
jennizalwango <jennyzalwango12@gmail.com>
2021-12-04 19:10:49 +0100

    add more test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,15 @@
-describe ('Home Page', ()=>{
+import Login from "../../todo-list/src/LoginForm"
+
+describe ('Home Page', ()=>
+{
   it('Successfully loads the home page', ()=>{
     cy.visit('http://localhost:3000/')
+  })
+})
+
+// test Navigation to  Login page
+describe('Login', ()=>{
+  it('Clicking on login  navigates to a new url', ()=>{
+    cy.url('include', '/login')
   })
 })
--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,40 +1,33 @@
 
-// // #dumysample tests
-// describe('My First Test', () => {
-//   it ('Doesnot do much!', () => {
-//     expect(true).to.equal(true)    
-//   })
-// })
+// #dumysample tests
+describe('My First Test', () => {
+  it ('Doesnot do much!', () => {
+    expect(true).to.equal(true)    
+  })
+})
 
-// //  wriet an Actualsample test
-// describe('Actaul Test', () =>{
-//   it('Visits the Kicthen Sink', () =>{
-//     // cy.visit('https://example.cypress.io')
-//   })
-// })
+// Query for an element
+describe('Testquery for an element', () =>{
+  it('finds the content "type"', () =>{
 
-// // Query for an element
-// describe('Testquery for an element', () =>{
-//   it('finds the content "type"', () =>{
+// add click method on the link we found 
 
-// // add click method on the link we found 
+    cy.contains('type').click()
+  })
+})
 
-//     cy.contains('type').click()
-//   })
-// })
+describe('still test', () => {
+  it('clicking "type" naviagets to a new url', () =>
+  {
+    cy.url().should('include', '/commands/actions')
 
-// describe('still test', () => {
-//   it('clicking "type" naviagets to a new url', () =>
-//   {
-//     cy.url().should('include', '/commands/actions')
+    // Get an input,type into it and verify that the value has been updated
 
-//     // Get an input,type into it and verify that the value has been updated
-
-//     cy.get('.action-email')
-//       .type('fake@gmail.com')
-//       .should('have.value', 'fake@gmail.com')
-//   })
-// })
+    cy.get('.action-email')
+      .type('fake@gmail.com')
+      .should('have.value', 'fake@gmail.com')
+  })
+})
 
 
 
